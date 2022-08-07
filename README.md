e0f36e264f088796137d8fffa261233925b47a26
Eugene Morozov <eugene.morozov@dalstonsemantics.com>
2022-08-07 10:23:47 +1000

    Initial commit.

---
+++ README.md
@@ -0,0 +1,4 @@
+# ISKO UK Subjects Taxonomy
+
+Taxonomy of subjects to index content of [ISKO UK](https://www.iskouk.org/) repository.
+
---
+++ subjects.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import rdfpandas
+from rdflib import Graph, Namespace
+from rdflib.namespace import NamespaceManager, SKOS, DCTERMS
+
+df = pd.read_csv('subjects.csv', index_col = '@id', keep_default_na = True, dtype = { 'pav:version': str })
+
+namespace_manager = NamespaceManager(Graph())
+namespace_manager.bind('skos', SKOS, override = True)
+namespace_manager.bind('iskouk', Namespace('https://iskouk.org/'), override = True)
+namespace_manager.bind('pav', Namespace('http://purl.org/pav/'), override = True)
+
+g = rdfpandas.to_graph(df, namespace_manager)
+
+ttl = g.serialize(format = 'turtle')
+with open('subjects.ttl', 'w') as file:
+   file.write(ttl)
